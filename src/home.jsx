c59e234b9298785c8e645835b51c37182fac8ec8
Nero Dicentra <nero.dicentra@gmail.com>
2023-09-27 03:00:11 +1000

    Add style reload button with svg

--- src/home.jsx
+++ src/home.jsx
@@ -2,6 +2,7 @@ import URLS from "./urls.js"
 import "./home.css"
 import Record from "./record.jsx"
 import { useStore } from "./store.jsx"
+import refreshIcon from "/refresh.svg"
 
 const Home = () => {
   let [reload, setReload] = useStore("reload")
@@ -10,13 +11,12 @@ const Home = () => {
     <div id="home">
       <header>
         <h1>Glimpse</h1>
-        <h1>
-          <input
-            type="button"
-            value="Reload"
-            onClick={() => setReload(!reload)}
-          />
-        </h1>
+        <img
+          onClick={() => setReload(!reload)}
+          src={refreshIcon}
+          className="icon"
+          alt="Refresh Icon"
+        />
       </header>
       <main>
         {URLS.map((url, inx) => (
